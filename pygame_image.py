059b6c2b6370edee485fbb179f45a878ea5579f2
kamata <c0c2401983@edu.teu.ac.jp>
2025-11-18 16:52:04 +0900

    練習9:背景永続コウカトン

--- pygame_image.py
+++ pygame_image.py
@@ -10,7 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    bg2_img = pg.transform.flip(bg_img, True, False)
+    bg_img2 = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     tmr = 0
@@ -18,8 +18,11 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
+        if tmr == 3199:
+            tmr = 0
         screen.blit(bg_img, [-tmr, 0])
-        screen.blit(bg2_img, [-tmr+1600,0])
+        screen.blit(bg_img2, [-tmr+1600,0])
+        screen.blit(bg_img, [-tmr+3200, 0])
         screen.blit(kk_img, [300,200])
         pg.display.update()
         tmr+=1
